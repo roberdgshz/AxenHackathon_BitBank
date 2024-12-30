82b2e92442e388b267f6bc5ebe5a2da3fb7beb65
roberdgshz <roberd.999117@gmail.com>
2024-12-30 13:59:19 -0600

    Auditlogs model

--- AxenHackathon/Django/_bitbank/appLogs/models.py
+++ AxenHackathon/Django/_bitbank/appLogs/models.py
@@ -2,3 +2,14 @@ from django.db import models
 from appAccounts.models import Account
 
 # Create your models here.
+class Auditlog(models.Model):
+    AuditlogID      = models.BigAutoField(primary_key=True)
+    AuditlogAccount = models.OneToOneField(
+        Account,
+        on_delete=models.CASCADE
+    )
+    AuditlogDescription = models.TextField()
+    AuditlogTime        = models.DateTimeField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.AuditlogID}, {self.AuditlogTime}"
