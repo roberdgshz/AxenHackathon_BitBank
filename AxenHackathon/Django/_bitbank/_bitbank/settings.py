97c70248fc1dcc0d66bcca17f733562f99a477eb
roberdgshz <roberd.999117@gmail.com>
2025-01-04 10:36:16 -0600

    Requirements

--- AxenHackathon/Django/_bitbank/_bitbank/settings.py
+++ AxenHackathon/Django/_bitbank/_bitbank/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -45,7 +47,46 @@ INSTALLED_APPS = [
     'appWallets',
     # Authentication for project
     # Allauth
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'allauth.socialaccount.providers.google',
+    'allauth.socialaccount.providers.github',
 ]
+
+#Authentication backends
+AUTHENTICATION_BACKENDS = [
+    'allauth.account.auth_backends.AuthenticationBackend',
+]
+
+google_id = os.environ.get('GOOGLE_CLIENT_ID')
+google_secret = os.environ.get('GOOGLE_SECRET')
+github_id = os.environ.get('GITHUB_CLIENT_ID')
+github_secret = os.environ.get('GITHUB_SECRET')
+
+SOCIALACCOUNT_PROVIDERS = {
+    'google': {
+        'APP': {
+            'client_id': google_id,
+            'secret': google_secret,
+          
+        },
+        'SCOPE': ['profile','email',],
+         'AUTH_PARAMS': {'access_type': 'online'},
+        'METHOD': 'oauth2',
+        'VERIFIED_EMAIL': True,
+    },
+    'github': {
+        'APP': {
+            'client_id': github_id,
+            'secret': github_secret,
+           
+        }
+    }
+   
+}
+
+AUTH_USER_MODEL = 'appAccounts.Account'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -55,6 +96,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'allauth.account.middleware.AccountMiddleware',
 ]
 
 ROOT_URLCONF = '_bitbank.urls'
@@ -129,3 +171,25 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#Email
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+
+email = os.environ.get("EMAIL")
+email_pass= os.environ.get("EMAIL_PASSWORD")
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = email
+EMAIL_HOST_PASSWORD = email_pass
+DEFAULT_FORM_EMAIL = email
+EMAIL_SUBJECT_PREFIX = "Password Recovery"
+
+SOCIALACCOUNT_LOGIN_ON_GET=True
+ACCOUNT_USER_MODEL_USERNAME_FIELD = None
+ACCOUNT_USERNAME_REQUIRED = False 
+ACCOUNT_EMAIL_REQUIRED = True  
+ACCOUNT_AUTHENTICATION_METHOD = "email" 
