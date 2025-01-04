72ebcd7dcacb5d35610b34dbac3915cc95515b19
Brandon Eduardo Reyes Hernández <brandonedu076@gmail.com>
2025-01-04 10:01:57 -0600

    Login Fix

--- AxenHackathon/Django/_bitbank/appAccounts/urls.py
+++ AxenHackathon/Django/_bitbank/appAccounts/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
 from .views import register, user_login, user_logout, user_home
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', user_home, name='home'),
     path('login/', user_login, name='login'),
     path('register/', register, name='register'),
     path('logout/', user_logout, name='logout'),
+
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='password_reset.html'), name='password_reset'),
+    path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(template_name='password_reset_done.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html'), name='password_reset_confirm'),
+    path('reset_done/', auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'), name='password_reset_complete'),
 ]
--- AxenHackathon/Django/_bitbank/appAccounts/views.py
+++ AxenHackathon/Django/_bitbank/appAccounts/views.py
@@ -3,25 +3,26 @@ from django.shortcuts import render
 # Create your views here.
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from .form import CustomUserCreationForm
 from django.contrib.auth import login
 from django.contrib.auth import authenticate, logout
 
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
             login(request, user)
             return redirect('home')
     else:
-        form = UserCreationForm()
+        form = CustomUserCreationForm()
     return render(request, 'users/register.html', {'form':form})
     
 def user_login(request):
     if request.method == 'POST':
-        username = request.POST['username']
+        email = request.POST['email']
         password = request.POST['password']
-        user = authenticate(request, username=username, password=password)
+        user = authenticate(request, username=email, password=password)
         if user is not None:
             login(request, user)
             return redirect('home')
