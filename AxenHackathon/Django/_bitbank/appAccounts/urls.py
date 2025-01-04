46f2913f2860b31a8b323a05acbc4452faf89266
roberdgshz <roberd.999117@gmail.com>
2025-01-04 17:48:16 -0600

    redirecction oAuth2

---
+++ AxenHackathon/Django/_bitbank/appAccounts/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('', ViewCompletation.as_view(), name="completation"),
+    path('/base', ViewBase.as_view(), name="base"),
+]
--- AxenHackathon/Django/_bitbank/appAccounts/views.py
+++ AxenHackathon/Django/_bitbank/appAccounts/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
+class ViewCompletation(TemplateView):
+    template_name = 'account/completation.html'
+
+class ViewBase(TemplateView):
+    template_name = 'base.html'
