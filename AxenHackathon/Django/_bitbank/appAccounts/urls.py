eff427e65ae2f4773ee5aba2e4c42dedf2d587a8
roberdgshz <roberd.999117@gmail.com>
2025-01-05 15:47:09 -0600

    oAuth2 redirect 2

--- AxenHackathon/Django/_bitbank/appAccounts/urls.py
+++ AxenHackathon/Django/_bitbank/appAccounts/urls.py
@@ -4,5 +4,5 @@ from .views import *
 urlpatterns = [
     path('', ViewCompletation.as_view(), name="completation"),
     path('base/', ViewBase.as_view(), name="base"),
-    path('validate-username/', ViewValidateUsername, name='validate_username'),
+    path('validate-username/', ViewRedirect, name='validate_username'),
 ]
--- AxenHackathon/Django/_bitbank/appAccounts/views.py
+++ AxenHackathon/Django/_bitbank/appAccounts/views.py
@@ -1,25 +1,14 @@
 from django.views.generic import TemplateView
-from django.shortcuts import render, get_object_or_404
-
+from django.shortcuts import render
 from .models import Account
-from django.http import JsonResponse
 
 # Create your views here.
 class ViewCompletation(TemplateView):
     template_name = 'account/completation.html'
 
-def ViewValidateUsername(request):
-    if request.method == 'POST':
-        username = request.POST.get('username', '').strip()
-        if not username:
-            return render(request, 'template.html', {'error': 'Username is required.'})
-
-        if Account.objects.filter(AccountUsername=username).exists():
-            return render(request, 'account/username_accounts_verify.html', {'error': 'Username already exists. Please choose another one.'})
-
-        return render(request, 'account/username_accounts_verify.html', {'error': 'Username is available!'})
-    
-    return render(request, 'account/username_accounts_verify.html')
+def ViewRedirect(request):
+    accounts = Account.objects.all()
+    return render(request, 'account/username_accounts_verify.html', {'accounts': accounts})
 
 class ViewBase(TemplateView):
     template_name = 'base.html'
