d190d0c25d4bbfdb98b2a5735bb4dc414465f984
roberdgshz <roberd.999117@gmail.com>
2025-01-13 13:15:13 -0600

    Transactions with notifications

--- AxenHackathon/Django/_bitbank/appTransactions/views.py
+++ AxenHackathon/Django/_bitbank/appTransactions/views.py
@@ -80,46 +80,52 @@ def ViewTransactionDepositGenerator(request):
             cursor.execute("SELECT walletcoinquantity FROM wallet WHERE walletaccountsid_id = %s AND walletcoinsid_id = %s",[id[0], currency])
             availableamount = cursor.fetchone()
         
-        if availableamount[0] >= Decimal(amount) :
-            with connection.cursor() as cursor: # This is to search the receiver's id
-                cursor.execute("SELECT accountid FROM account WHERE accountusername = %s",[receiver])
-                idreceiver = cursor.fetchone()
+        if availableamount :
+            if availableamount[0] >= Decimal(amount) :
+                with connection.cursor() as cursor: # This is to search the receiver's id
+                    cursor.execute("SELECT accountid FROM account WHERE accountusername = %s",[receiver])
+                    idreceiver = cursor.fetchone()
 
-            with connection.cursor() as cursor: # This is to search if the receiver already has a wallet with the specific coin
-                cursor.execute("SELECT * FROM wallet WHERE walletcoinsid_id = %s AND walletaccountsid_id = %s",[currency, idreceiver[0]])
-                walletreceiver = cursor.fetchone()
+                with connection.cursor() as cursor: # This is to search if the receiver already has a wallet with the specific coin
+                    cursor.execute("SELECT * FROM wallet WHERE walletcoinsid_id = %s AND walletaccountsid_id = %s",[currency, idreceiver[0]])
+                    walletreceiver = cursor.fetchone()
 
-            with connection.cursor() as cursor: # This is to get the value of the coin
-                cursor.execute("SELECT coinvalue FROM coin WHERE coinid = %s",[currency])
-                coinvalue = cursor.fetchone()
+                with connection.cursor() as cursor: # This is to get the value of the coin
+                    cursor.execute("SELECT coinvalue FROM coin WHERE coinid = %s",[currency])
+                    coinvalue = cursor.fetchone()
 
-            if walletreceiver:
-                with connection.cursor() as cursor: # This is to search the available's quantity of the receiver
-                    cursor.execute("SELECT walletcoinquantity FROM wallet WHERE walletaccountsid_id = %s AND walletcoinsid_id = %s",[idreceiver[0], currency])
-                    totalamount = cursor.fetchone()
+                if walletreceiver:
+                    with connection.cursor() as cursor: # This is to search the available's quantity of the receiver
+                        cursor.execute("SELECT walletcoinquantity FROM wallet WHERE walletaccountsid_id = %s AND walletcoinsid_id = %s",[idreceiver[0], currency])
+                        totalamount = cursor.fetchone()
 
-                transmitternewavailableamount = availableamount[0] - Decimal(amount)
-                receivernewtotalamount = totalamount[0] + Decimal(amount)
+                    transmitternewavailableamount = availableamount[0] - Decimal(amount)
+                    receivernewtotalamount = totalamount[0] + Decimal(amount)
 
-                transmitternewbalance = transmitternewavailableamount * coinvalue[0]
-                receivernewbalance = receivernewtotalamount * coinvalue[0]
+                    transmitternewbalance = transmitternewavailableamount * coinvalue[0]
+                    receivernewbalance = receivernewtotalamount * coinvalue[0]
 
-                with connection.cursor() as cursor: # This is to alter the table with the new quantity's and values
-                    cursor.execute("SELECT alter_wallet(%s,%s,%s,%s,%s,%s,%s)",[currency,id[0],idreceiver[0],transmitternewavailableamount,receivernewtotalamount,transmitternewbalance,receivernewbalance])
-            else :
-                receiverbalance = coinvalue[0] * Decimal(amount)
-                
-                transmitternewavailableamount = availableamount[0] - Decimal(amount)
-                transmitternewbalance = transmitternewavailableamount * coinvalue[0]
+                    with connection.cursor() as cursor: # This is to alter the table with the new quantity's and values
+                        cursor.execute("SELECT alter_wallet(%s,%s,%s,%s,%s,%s,%s)",[currency,id[0],idreceiver[0],transmitternewavailableamount,receivernewtotalamount,transmitternewbalance,receivernewbalance])
 
-                with connection.cursor() as cursor:
-                    cursor.execute("SELECT insert_wallet(%s,%s,%s,%s)",[receiverbalance,idreceiver[0],currency,amount])
-                
-                with connection.cursor() as cursor:
-                    cursor.execute("UPDATE wallet SET walletquantity = %s, walletbalance = %s WHERE walletaccountsid_id = %s AND walletcoinsid_id = %s",[transmitternewavailableamount,transmitternewbalance,currency,id[0]])
-        else:
+                    
+                    with connection.cursor() as cursor:
+                        cursor.execute("SELECT insert_notification_transaction(%s,%s,%s)",[])
+                else :
+                    receiverbalance = coinvalue[0] * Decimal(amount)
+                    
+                    transmitternewavailableamount = availableamount[0] - Decimal(amount)
+                    transmitternewbalance = transmitternewavailableamount * coinvalue[0]
+
+                    with connection.cursor() as cursor:
+                        cursor.execute("SELECT insert_wallet(%s,%s,%s,%s)",[receiverbalance,idreceiver[0],currency,amount])
+                    
+                    with connection.cursor() as cursor:
+                        cursor.execute("UPDATE wallet SET walletcoinquantity = %s, walletbalance = %s WHERE walletaccountsid_id = %s AND walletcoinsid_id = %s",[transmitternewavailableamount,transmitternewbalance,id[0],currency])
+            else:
+                messages.error(request, "Not enough quantity available!")
+        else :
             messages.error(request, "Not enough quantity available!")
-            return render(request, 'transactions/transaction_deposit.html')
     return render(request, 'transactions/transaction_deposit_generator.html')
 
 def ViewTransactionReceiveGenerator(request):
@@ -133,6 +139,28 @@ def ViewTransactionReceiveGenerator(request):
             id = cursor.fetchone()
 
         with connection.cursor() as cursor:
-            cursor.execute("SELECT insert_transaction_folio(%s,%s,%s)",[id[0],amount,currency])
+            cursor.execute("SELECT coinvalue FROM coin WHERE coinid = %s",[currency])
+            coinid = cursor.fetchone()
+
+        balance = coinid[0] * Decimal(amount)
+
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT * FROM wallet WHERE walletcoinsid_id = %s AND walletaccountsid_id = %s",[currency,id])
+            wallet = cursor.fetchone()
+
+        if wallet :
+
+            newquantity = wallet[4] + Decimal(amount)
+            newbalance = newquantity * coinid[0]
+            
+            with connection.cursor() as cursor:
+                cursor.execute("UPDATE wallet SET walletcoinquantity = %s, walletbalance = %s WHERE walletaccountsid_id = %s AND walletcoinsid_id = %s", [newquantity,newbalance,id,currency])
+            
+        else:
+            with connection.cursor() as cursor:
+                cursor.execute("SELECT insert_wallet(%s,%s,%s,%s)",[balance,id,currency,amount])
+        
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT insert_transaction_folio(%s,%s,%s)",[id,amount,currency])
 
     return render(request, 'transactions/transaction_receive_generator.html')
